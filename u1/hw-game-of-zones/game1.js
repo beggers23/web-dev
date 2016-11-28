0513f657a440bd3e59bf1b1b036c0e51e8d3346b
Dev Eggers <eggers.brendan@gmail.com>
2016-11-28 10:16:08 -0500

    Commits the move from the forked repo to the standalone repo

--- u1/hw-game-of-zones/game1.js
+++ u1/hw-game-of-zones/game1.js
@@ -1,1 +1,98 @@
 console.log('Linked');
+
+//any iteration, use let
+//any hold variable use const
+const game = {
+  title: 'Game of Zones',
+};
+
+
+//Game One Psedo Code
+
+
+$(document).ready(function(){
+  // $('body').mousemove(function(e){
+  //   console.log('moving mouse');
+  // })//Get some verification that mouse move works.
+
+  /////////////////////////////////////////////
+  //Change mouse move to mouse enter/mouse leave on the boxes
+  // $('.zone').mouseenter(function(e){
+  //   console.log(e.target.id);
+  // });
+  // $('.zone').mouseleave(function(e){
+  //   console.log(e.target.id);
+  // });
+
+  /////////////////////////////////////////////
+  //Get the background of the boxes to change to green when the mouse enters
+  $('.zone').mouseenter(function(e){
+    const target = $('#'+e.target.id);
+    if(target.attr('class') != 'zone green'){
+      target.addClass('green');
+      target.removeClass('white');
+    }
+  });
+
+  /////////////////////////////////////////////
+  //Then get the boxes to change back to white when the mouse leaves the box.
+
+  $('.zone').mouseleave(function(e){
+    //Only allow the background to change if the zone does not have the class of green
+    const target = $('#'+e.target.id);
+    if(target.attr('class') != 'zone green clicked'){
+      target.addClass('white');
+      target.removeClass('green');
+    }
+  });
+
+  /////////////////////////////////////////////
+  //When the box is clicked, add the green class to the box
+  $('.zone').click(function(e){
+    const target = $('#'+e.target.id);
+    target.toggleClass('clicked');
+    game.checkStatus();
+  });
+
+  /////////////////////////////////////////////
+  //When all zones are checked green, create a congratulations message to be sent to the console
+
+  game.checkStatus = function(){
+    game.score = $('.zone.green').length;
+    if(game.score === 4){
+      console.log('YOU WIN!');
+      game.makePretty();
+    }
+  }
+
+  /////////////////////////////////////////////
+  game.makePretty = function(){
+    const zones = $('.zone');
+    for(let i=0; i<zones.length; i++){
+      setTimeout(function(){
+        $(zones[i]).removeClass('green').addClass('yellow');
+      }, 1000);
+
+      setTimeout(function(){
+        $(zones[i]).removeClass('yellow').addClass('red');
+      }, 2000);
+
+      setTimeout(function(){
+        $(zones[i]).removeClass('red clicked').addClass('white');
+      }, 3000);
+    }
+  }
+
+
+  /////////////////////////////////////////////
+  //Allows game to be reset back to blank zones
+  $('#reset').click(function(e){
+    let zones = $('.zone');
+    for(let i=0; i<zones.length;i++){
+      if($(zones[i]).attr('class') === 'zone green clicked'){
+        $(zones[i]).removeClass('green clicked');
+      }
+    }
+  });
+
+});
--- u1/hw-game-of-zones/game2.js
+++ u1/hw-game-of-zones/game2.js
@@ -1,1 +1,112 @@
 console.log('game2.js linked');
+
+let game = {
+  title: 'Game of Zones',
+  zones: ['zone-1', 'zone-2','zone-3','zone-4'],
+  score: 0,
+};
+
+///////////////////////////////////////
+//Shuffles the order of the zones.
+
+game.shuffle = function(){
+  const array = game.zones;
+  for(let i=array.length-1; i>0; i--){
+    let j = Math.floor(Math.random() * (i+1));
+    let temp = array[i];
+    array[i] = array[j];
+    array[j] = temp;
+  }
+}
+
+
+
+game.checkZone = function(){
+  // With the order shuffled, if the zone that is hovered is
+  $('.zone').mouseenter(function(e){
+    const goal = e.target.id;
+    const target = $('#'+e.target.id);
+    //the first place in the ordered array,
+    if(target.attr('class') != 'zone green clicked'){
+      if(goal === game.zones[game.score]){
+        // then it should turn green
+        target.addClass('green');
+        target.removeClass('white');
+        //And be allowed to be clicked
+        game.allowClick(target);
+      }
+      else{
+        //If not, it should turn red
+        target.addClass('red');
+        target.removeClass('white');
+      }
+    }
+  });
+  //When the mouse leaves, change the color back to white
+  $('.zone').mouseleave(function(e){
+    const goal = e.target.id;
+    const target = $('#'+e.target.id);
+    if(target.attr('class') != 'zone green clicked'){
+      target.addClass('white');
+      target.removeClass('green');
+      target.removeClass('red');
+    }
+  })
+}
+//Allows the click function to the target zone
+game.allowClick = function(target){
+  target.click(function(e){
+    //When Clicked it adds the class of click and updates the score of the game
+    target.toggleClass('clicked');
+    game.score = $('.zone.green.clicked').length;
+    if(game.score === 4){
+      //Game is complete when the score equals 4 and starts to turn the zones back to white
+      console.log('YOU WIN!');
+      game.resetZones(0);
+    }
+  });
+}
+//Will change green zones to red, red zones to white
+game.resetZones = function(i){
+  let greenTarget = $('#'+game.zones[i]);
+  let redTarget = $('#'+game.zones[i-1]);
+
+  //It needs to run every one second
+
+  //When resetZones is called, I pass through 0 as i so it will start with the first zone in the array of shuffled zones. Then every second it will call itself after changing the first green box to red. Then it will change the next green box to red, and the first red box to green and so on.
+  game.interval = setTimeout(function(){
+    if(greenTarget.attr('class') === 'zone green clicked'){
+      greenTarget.removeClass('green clicked');
+      greenTarget.addClass('red');
+      game.resetZones(i+1);
+    }
+    if(redTarget.attr('class') === 'zone red'){
+      redTarget.removeClass('red');
+    }
+  }, 1000);
+
+  if(i==3){
+    //if i reaches the length of the zones array call reset after 1 second so the last redZone can be changed to white
+    setTimeout(function(){
+      game.reset();
+    },1000);
+  }
+}
+
+
+//Clears the interval from reset
+game.reset = function(){
+  game.interval = 0;
+  //reshuffles the zones
+  game.shuffle();
+
+  //sets the score back to 0
+  game.score = 0;
+}
+
+
+//shuffles the zones and starts the game
+$(document).ready(function(){
+  game.shuffle();
+  game.checkZone();
+})
