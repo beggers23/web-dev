a8aefff4975b4b449df21c1d7da0b88207303c34
Dev Eggers <eggers.brendan@gmail.com>
2016-12-01 13:15:28 -0500

    Commits adding of more information about the searched movie

--- u2/hw-movieHaus/client/js/app.js
+++ u2/hw-movieHaus/client/js/app.js
@@ -20,17 +20,81 @@ movieHaus.getMovie = function(search){
 
 movieHaus.displayMovie = function(movie){
   console.log(movie);
-  //Sets up the title
+
+  //Sets up the title h1
+  movieHaus.movieHeader(movie);
+
+  //Sets up the details, actor, director and others
+  movieHaus.movieDetails(movie);
+
+  //Sets up the Plot description
+  movieHaus.moviePlot(movie);
+
+  //Sets up the Details about Actors and Directors
+  movieHaus.movieCast(movie);
+
+  //Creates poster img
+  movieHaus.moviePoster(movie);
+}
+
+movieHaus.movieHeader = function( movie ){
+  let titleBox = document.createElement('div');
+  titleBox.className = 'titleBox';
+  document.querySelector('#textSpot').appendChild(titleBox);
+
   let title = document.createElement('h1');
   title.appendChild(document.createTextNode(movie.Title));
-  //Creates poster img
+  title.className = 'title';
+
+  let year = document.createElement('span');
+  year.className = 'year';
+  year.appendChild(document.createTextNode(' ('+movie.Year+') ['+movie.Rated+']'));
+  title.appendChild(year);
+
+  titleBox.appendChild(title);
+}
+
+movieHaus.movieDetails = function(movie){
+  let detailsBox = document.createElement('div');
+  detailsBox.className = 'detailsBox';
+  document.querySelector('#textSpot').appendChild(detailsBox);
+
+  let metaScore = document.createElement('h4');
+  metaScore.className = 'metaScore';
+  metaScore.appendChild(document.createTextNode('Metascore: '+movie.Metascore));
+
+  let imdbRating = document.createElement('h4');
+  imdbRating.className = 'imdbRating';
+  imdbRating.innerHTML = '<a class="ratingLink" href="http://www.imdb.com/title/'+movie.imdbID+'">IMDB Rating: '+movie.imdbRating+'</a>';
+
+  detailsBox.appendChild(metaScore);
+  detailsBox.appendChild(imdbRating);
+
+}
+
+
+movieHaus.moviePlot = function(movie){
+  let plot = document.createElement('p');
+  plot.className = 'moviePlot';
+  plot.appendChild(document.createTextNode(movie.Plot));
+
+  document.querySelector('#textSpot').appendChild(plot);
+}
+
+movieHaus.movieCast = function(movie){
+
+}
+
+
+movieHaus.moviePoster = function(movie){
   let poster = document.createElement('img');
+  poster.className = 'moviePoster';
   poster.src = movie.Poster;
   poster.title = movie.Title +' ('+movie.Year+')';
-  poster.alt = 'Poster Image for '+movie.Title
+  poster.alt = 'Poster Image for '+movie.Title;
 
+  //Appends assignments to appropriate locations
   document.querySelector('#posterSpot').appendChild(poster);
-  document.querySelector('#textSpot').appendChild(title);
 }
 
 document.addEventListener('DOMContentLoaded', ()=>{
